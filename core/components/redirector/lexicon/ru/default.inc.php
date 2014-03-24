632fd7bba2de618755b9a475b9e4874fcdb82d47
DanyaPostfactum <danya.postfactum@gmail.com>
2014-03-24 19:11:15 +1000

    Localize new strings in the ru lexicon

--- core/components/redirector/lexicon/ru/default.inc.php
+++ core/components/redirector/lexicon/ru/default.inc.php
@@ -7,16 +7,17 @@
  */
 $_lang['redirector'] = 'Redirector';
 $_lang['redirector.active'] = 'Включён';
-$_lang['redirector.desc'] = 'Управление редиректами на вашем сайте. Red lines means that the pattern URL is still exists OR target URL doesn\'t exists for a resource and will not be redirected.';
-$_lang['redirector.nothing_found'] = 'No redirects found yet!';
+$_lang['redirector.desc'] = 'Управление редиректами на вашем сайте. Красным цветом отмечены редиректы с некорректным шаблоном (в качестве шаблона указан еще существующий URL) или несуществующим целевым URL. Такие редиректы не будут работать.';
+$_lang['redirector.nothing_found'] = 'Редиректов пока нет!';
 $_lang['redirector.description'] = 'Описание';
 $_lang['redirector.redirect_err_ae'] = 'Редирект с таким именем уже есть.';
-$_lang['redirector.redirect_err_ae_patctx'] = 'A redirect with this pattern and "[[+context]]" context already exists.';
-$_lang['redirector.redirect_err_ae_uri'] = 'URI already exists for Resource ID [[+id]] in "[[+context]]" context... Redirect will not work!';
+$_lang['redirector.redirect_err_ae_pattern'] = 'Редирект с таким шаблоном уже есть.';
+$_lang['redirector.redirect_err_ae_patctx'] = 'Редирект с таким шаблоном и контекстом "[[+context]]" уже есть.';
+$_lang['redirector.redirect_err_ae_uri'] = 'URI уже существует (ресурс с ID [[+id]] в контексте "[[+context]]")... Редирект не будет работать...';
 $_lang['redirector.redirect_err_nf'] = 'Редирект не найден.';
 $_lang['redirector.redirect_err_ns'] = 'Редирект не указан.';
 $_lang['redirector.redirect_err_ns_name'] = 'Укажите имя редиректа.';
-$_lang['redirector.redirect_err_ne_target'] = 'Target URI doesn\'t exists... Redirect won\'t work...';
+$_lang['redirector.redirect_err_ne_target'] = 'Целевой URI не существет... Редирект не будет работать...';
 $_lang['redirector.redirect_err_remove'] = 'Произошла ошибка при попытке удалить редирект.';
 $_lang['redirector.redirect_err_save'] = 'Произошла ошибка при попытке сохранить редирект.';
 $_lang['redirector.redirect_create'] = 'Создать новый редирект';
@@ -29,29 +30,29 @@ $_lang['redirector.menu_desc'] = 'Управление редиректами.';
 $_lang['redirector.pattern'] = 'Шаблон';
 $_lang['redirector.search...'] = 'Поиск...';
 $_lang['redirector.target'] = 'Цель';
-$_lang['redirector.context'] = 'Context';
-$_lang['redirector.context_desc'] = 'If context is set, redirect only affects on loaded context.';
+$_lang['redirector.context'] = 'Контекст';
+$_lang['redirector.context_desc'] = 'Если контекст указан, редирект будет работать только когда загружен указанный контекст.';
 
-$_lang['redirector.import'] = 'Import CSV';
-$_lang['redirector.import_desc'] = 'Here you can import new rules provided in CSV format. You can upload a CSV file or past raw CSV format in the textarea below';
-$_lang['redirector.import.csv_desc'] = 'Notice: format of the CSV must be "pattern;target;context", where "context" may be skipped or empty.<br/>Also; use relative URLs, not include [[+site_url]] or similar ones.';
-$_lang['redirector.import.csv_file'] = 'CSV File selection';
-$_lang['redirector.import.raw_csv'] = 'Paste raw CSV data here';
-$_lang['redirector.import.do'] = 'Start Import!';
-$_lang['redirector.import.doing'] = 'Busy with importing CSV data...';
-$_lang['redirector.import.success'] = 'Total [[+total]] records found. Imported: [[+succeed]] successful and [[+failed]] failed!';
-$_lang['redirector.import.failed'] = 'Failed importing CSV data... Try again!';
+$_lang['redirector.import'] = 'Импорт CSV';
+$_lang['redirector.import_desc'] = 'Здесь вы можете импортировать новые правила в формате CSV. Вы можете загрузить CSV файл или вставить CSV данные в текстовую область.';
+$_lang['redirector.import.csv_desc'] = 'Обратите внимание: CSV данные должны соответствовать формату "шаблон;цель;контекст", где "контекст" может отсутствовать или остаться пустым.<br/>Также URL-ссылки должны быть относительными, не включать в себя [[+site_url]] и т.п.';
+$_lang['redirector.import.csv_file'] = 'Выбрать CSV файл';
+$_lang['redirector.import.raw_csv'] = 'Вставить CSV данные';
+$_lang['redirector.import.do'] = 'Начать импорт!';
+$_lang['redirector.import.doing'] = 'Идет импорт CSV данных...';
+$_lang['redirector.import.success'] = 'Найдено [[+total]] записей. Импортировано: [[+succeed]] успешно и [[+failed]] неудачно!';
+$_lang['redirector.import.failed'] = 'Импорт CSV закончился неудачно... Попробуйте снова!';
 
-$_lang['redirector.regex_explain'] = 'You can use regular expressions in the pattern and retrieve your replacements back in your target URL.
+$_lang['redirector.regex_explain'] = 'Вы можете использовать в шаблоне регулярные выражения с групповыми подвыражениями и ссылаться на группы в целевом URL.
 
-For example; you have many old URLs like "shop/category-{name}/" and you want to redirect all those URLs to the new "webshop/{name}/" structure, no problem!
-Enter these values:
+Например: у вас есть старые ссылки вида "shop/category-{name}/" и вы хотите перенаправить их на новые в формате "webshop/{name}/".
+Создайте такой редирект:
 
-<b>Pattern:</b> ^shop\/category-(.*)\/$
-<b>Target:</b> webshop/$1/
+<b>Шаблон:</b> ^shop\/category-(.*)\/$
+<b>Цель:</b> webshop/$1/
 
-You can also apply more wildcards (.*) and use $2, $3 etc. And as you can see, you need to escape the forward slashes and define a ^ for the start and $ for the end position.';
+Вы можете использовать несколько групповых подстановок (.*) и ссылаться на них через $2, $3 и т.д. Обратите внимание: при использовании регулярных выражений необходимо экранировать слэш-символ /, а также использовать символ начала строки ^ и символ конца строки $ .';
 
 // settings
-$_lang['setting_redirector.track_uri_updates'] = 'Track URI Updates';
-$_lang['setting_redirector.track_uri_updates_desc'] = 'If enabled, this will keep track on resource URI updates. Automatically created redirects for old > new URIs.';
+$_lang['setting_redirector.track_uri_updates'] = 'Отслеживать обновления URI';
+$_lang['setting_redirector.track_uri_updates_desc'] = 'Если включено, будут отслеживаться обновления URI ресурсов и автоматически создаваться соответствующие редиректы (со старого URI на новый).';
